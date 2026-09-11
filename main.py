26847705f879bfc88edc0b87ebcaaa473103fcc1
bpgigi <3046738904@qq.com>
2026-09-11 18:32:22 +0800

    上传readme和学习py的文件读写功能

--- main.py
+++ main.py
@@ -21,5 +21,27 @@ edu = EduManagement()
 # file = open("使用说明.txt","r",encoding="utf-8")
 # print(file.read())
 # file.close()
-with open("使用说明.txt","r",encoding="utf-8") as f:
-    print(f.read())
+# with open("使用说明.txt","r",encoding="utf-8") as f:
+#     print(f.read())
+# file = open('使用说明.txt', 'r', encoding='utf-8')
+# for line in file:
+#     print(line, end='')
+# file.close()
+# file = open("使用说明.txt","r",encoding="utf-8")
+# lines = file.readlines()
+# for line in lines:
+#     print(line,end="")
+# file.close()
+file = open("学生信息.txt","a",encoding="utf-8")
+# file.write("\n")
+# file.write(input("\n请输入学生名字"))
+#file.write("\n",input("\n请输入学生名字"))
+#一下为gpt教我的
+# name = input("请输入学生名字")
+# file.write("\n" + name)
+# file.write("\n" + input("输入"))
+# file.write(f"\n{input("ad")}") --->单引号双引号匹配错误
+file.write(f"\n{input('ad')}")
+file.close()
+f = open("学生信息.txt","r",encoding="utf-8")
+print(f.read())
