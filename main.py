635e1274bf0b0b748a2eaad6e721461dce129530
bpgigi <3046738904@qq.com>
2026-09-11 20:46:16 +0800

    异常初试 on PC

--- main.py
+++ main.py
@@ -32,16 +32,32 @@ edu = EduManagement()
 # for line in lines:
 #     print(line,end="")
 # file.close()
-file = open("学生信息.txt","a",encoding="utf-8")
-# file.write("\n")
-# file.write(input("\n请输入学生名字"))
-#file.write("\n",input("\n请输入学生名字"))
-#一下为gpt教我的
-# name = input("请输入学生名字")
-# file.write("\n" + name)
-# file.write("\n" + input("输入"))
-# file.write(f"\n{input("ad")}") --->单引号双引号匹配错误
-file.write(f"\n{input('ad')}")
-file.close()
-f = open("学生信息.txt","r",encoding="utf-8")
-print(f.read())
+# file = open("学生信息.txt","a",encoding="utf-8")
+# # file.write("\n")
+# # file.write(input("\n请输入学生名字"))
+# #file.write("\n",input("\n请输入学生名字"))
+# #一下为gpt教我的
+# # name = input("请输入学生名字")
+# # file.write("\n" + name)
+# # file.write("\n" + input("输入"))
+# # file.write(f"\n{input("ad")}") --->单引号双引号匹配错误
+# file.write(f"\n{input('ad')}")
+# file.close()
+# f = open("学生信息.txt","r",encoding="utf-8")
+# print(f.read())
+
+#异常
+file = None
+try:
+    file = open('lesson01.txt', 'r', encoding='utf-8')
+    print(file.read())
+except FileNotFoundError:
+    print("无法打开指定文件!")
+except LookupError:
+    print("指定了位置编码")
+except UnicodeDecodeError:
+    print("读取文件是解码错误")
+finally:
+    if file:
+        file.close()
+
